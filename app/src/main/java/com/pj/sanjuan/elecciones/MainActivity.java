5b32d43afb88ecf365c4550ef434456339941e48
juanmanuelariza <juanma.ariza23@gmail.com>
2018-06-24 23:54:36 -0300

    Initial commit

---
+++ app/src/main/java/com/pj/sanjuan/elecciones/MainActivity.java
@@ -0,0 +1,50 @@
+package com.pj.sanjuan.elecciones;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextPhone);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, IndexActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
+    }
+
+    public void startVerification(View s){
+        String number = editText.getText().toString().trim();
+
+        if (number.isEmpty() || number.length() < 10) {
+            editText.setError("");
+            editText.requestFocus();
+            return;
+        }
+
+        String phoneNumber = "+54" + number;
+
+        Intent intent = new Intent(MainActivity.this, VerifyPhoneActivity.class);
+        intent.putExtra("phonenumber", phoneNumber);
+        startActivity(intent);
+    }
+}
